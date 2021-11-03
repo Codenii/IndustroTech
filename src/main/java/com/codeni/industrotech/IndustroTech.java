8f91b315eb1611a59c4704f799b4ff0303f10593
Codenii <codeni80@gmail.com>
2021-11-03 17:51:02 -0400

    Added custom ItemGroups for mod items.

--- src/main/java/com/codeni/industrotech/IndustroTech.java
+++ src/main/java/com/codeni/industrotech/IndustroTech.java
@@ -3,9 +3,33 @@ package com.codeni.industrotech;
 import com.codeni.industrotech.registry.ModBlocks;
 import com.codeni.industrotech.registry.ModItems;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.util.Identifier;
 
 public class IndustroTech implements ModInitializer {
     public static final String MOD_ID ="indutech";
+
+    public static final ItemGroup ITEM_GROUP = FabricItemGroupBuilder.build(
+            new Identifier(MOD_ID, "general"),
+            () -> new ItemStack(ModItems.RUBY)
+    );
+
+    public static final ItemGroup OTHER_GROUP = FabricItemGroupBuilder.create(
+            new Identifier(MOD_ID, "other"))
+            .icon(() -> new ItemStack(Blocks.ENCHANTING_TABLE))
+            .appendItems(stacks -> {
+                stacks.add(new ItemStack(ModBlocks.RUBY_BLOCK));
+                stacks.add(new ItemStack(Items.APPLE));
+                stacks.add(new ItemStack(ModItems.RUBY));
+                stacks.add(new ItemStack(Items.GLOWSTONE_DUST));
+            })
+            .build();
+
     @Override
     public void onInitialize() {
         ModItems.registerItems();
--- src/main/java/com/codeni/industrotech/registry/ModItems.java
+++ src/main/java/com/codeni/industrotech/registry/ModItems.java
@@ -9,11 +9,11 @@ import net.minecraft.util.registry.Registry;
 
 public class ModItems {
     //Items
-    public static final Item RUBY = new Item(new Item.Settings().group(ItemGroup.MATERIALS));
+    public static final Item RUBY = new Item(new Item.Settings().group(IndustroTech.ITEM_GROUP));
 
 
     //Blocks
-    public static final BlockItem RUBY_BLOCK = new BlockItem(ModBlocks.RUBY_BLOCK, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS));
+    public static final BlockItem RUBY_BLOCK = new BlockItem(ModBlocks.RUBY_BLOCK, new Item.Settings().group(IndustroTech.ITEM_GROUP));
 
     public static void registerItems() {
         Registry.register(Registry.ITEM, new Identifier(IndustroTech.MOD_ID, "ruby"), RUBY);
